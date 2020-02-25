1afa603cd447a77fc97d29bd561f9da1906ffbb6
Bosco Sam <boscos99@hotmail.com>
2020-02-25 13:36:07 -0800

    error MSB3644 is cleared after Workloads installed Data science and analytical applications with Visual Studio Installer.

--- LearningDjango01/HelloDjangoApp/views.py
+++ LearningDjango01/HelloDjangoApp/views.py
@@ -1,6 +1,47 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Django")
+    
+    now = datetime.now()
+
+    #html_content="<html><head><title>Hello, Django</title></head><body>"
+
+    #html_content+="<strong>Hello Django!</strong></br>"
+    #html_content+=now.strftime("%A, %d %B, %Y at %X")
+
+    #html_content+="</body></html>"
+
+    return render( 
+        request, "HelloDjangoApp/index.html", {
+            'title': "Hello Django",
+            'content':'This is the current time on server: ',
+            'current_time': now.strftime("%A, %d %B, %Y at %X"),
+            }
+        )
+
+#def index(request):
+
+    #return render(
+    #    request,
+    #    "HelloDjangoApp/index.html",  
+    #    # Relative path from the 'templates' folder to the template file
+    #    # "index.html", 
+    #    # Use this code for VS 2017 15.7 and earlier
+    #    {
+    #        'content': "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    #    }
+    #)
+
+#def index(request):
+#    now = datetime.now()
+#    html_body_code = now.strftime("%A, %d %B, %Y at %X")
+#    return render(
+#        request,
+#        'HelloDjangoApp/index.html', context = {
+#            'content': html_body_code,
+#        }, content_type = "text/html", 
+#    )
