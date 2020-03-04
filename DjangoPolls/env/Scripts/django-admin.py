cea5b83992d2e22b3cc2f1c986573844ab979b09
Bosco Sam <boscos99@hotmail.com>
2020-03-04 10:34:48 -0800

    completed Step 6-1  Create the project and initialize the database  https://docs.microsoft.com/en-us/visualstudio/python/learn-django-in-visual-studio-step-06-polls-django-web-project-template?view=vs-2019#step-6-1-create-the-project-and-initialize-the-database

---
+++ DjangoPolls/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\examples\Python\LearningDjango01\DjangoPolls\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
